4dcf592fdd9c72b0fdef79b52f97edd41fd9a7ff
UrayLaszlo <uray077@gmail.com>
2022-11-29 14:37:47 +0100

    Debugged Python script

--- foo.py
+++ foo.py
@@ -2,17 +2,19 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": 1,
-   "id": "3ec97858",
+   "execution_count": 210,
+   "id": "dd147dca",
    "metadata": {},
    "outputs": [
     {
-     "ename": "SyntaxError",
-     "evalue": "invalid syntax (1127610346.py, line 12)",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[0;36m  File \u001b[0;32m\"/var/folders/06/kkh22cm93nsbkc86_qszf_l80000gp/T/ipykernel_1428/1127610346.py\"\u001b[0;36m, line \u001b[0;32m12\u001b[0m\n\u001b[0;31m    if (is_divisible_by_k(x, 2) & is_divisible_by_k(x, 3)) |\u001b[0m\n\u001b[0m                                                            ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m invalid syntax\n"
-     ]
+     "data": {
+      "text/plain": [
+       "328927"
+      ]
+     },
+     "execution_count": 210,
+     "metadata": {},
+     "output_type": "execute_result"
     }
    ],
    "source": [
@@ -20,16 +22,15 @@
     " '''\n",
     " Checks whether x is divisible by k.\n",
     " '''\n",
-    " assert x%k == 0\n",
+    " return x%k == 0\n",
     "'''\n",
     "Store all the integers that are multiples of 2 or 5 or 7 that are lower\n",
     "or equal to 1000 (excluding doubles)\n",
     "'''\n",
-    "x = ()\n",
-    "for i in range(1000):\n",
-    " if (is_divisible_by_k(x, 2) & is_divisible_by_k(x, 3)) |\n",
-    "is_divisible_by_k(x, 7):\n",
-    " x.append(i)\n",
+    "x = []\n",
+    "for i in range(1001):\n",
+    " if (is_divisible_by_k(i, 2) or is_divisible_by_k(i, 5)) or is_divisible_by_k(i, 7):\n",
+    "     x.append(i)\n",
     "\n",
     "'''\n",
     "Sum all the integers that are multiples of 2 or 5 or 7 that are lower\n",
@@ -41,7 +42,7 @@
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "e06925df",
+   "id": "dbce5aa7",
    "metadata": {},
    "outputs": [],
    "source": []
